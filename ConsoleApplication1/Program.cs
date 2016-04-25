b1c912e5cf27bc26fe115216afc0229c2c19b527
matthew <properties@users.noreply.github.com>
2016-04-25 21:59:00 +0200

    Removed the stopwatch of @hex
    replaced stopwatch with regular expressions m8

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -62,13 +62,22 @@ namespace ConsoleApplication1
                 goto doAgain;
             }
 
-
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            // the code that you want to measure comes here
             var request = (HttpWebRequest)WebRequest.Create("http://www.leakedsource.com/");
 
-            var postData = $"&search={searchString}";postData += $"&searchType={searchType}";postData += $"&submit=Search";if (wildCard == "y") postData += $"&wildCard=true";
-            var data = Encoding.ASCII.GetBytes(postData);request.Method = "POST";request.ContentType = "application/x-www-form-urlencoded";request.ContentLength = data.Length;
+            var postData = $"&search={searchString}";
+            postData += $"&searchType={searchType}";
+            postData += $"&submit=Search";
+            
+            if (wildCard == "y") 
+            {
+                postData += $"&wildCard=true";
+            }
+            
+            var data = Encoding.ASCII.GetBytes(postData);
+            
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
 
             using (var stream = request.GetRequestStream())
             {
@@ -85,10 +94,15 @@ namespace ConsoleApplication1
 
                 string rektHtml = Regex.Replace(responseString.Replace("<br>", "\r\n"), @"<[^>]*?>", "");
 
-                string RegMatch = "seconds.(.*?)function";
-                if (rektHtml.Contains("The following results are provided free of charge!"))
+                string RegMatch = "onds.(.*?)func";
+                if (rektHtml.Contains("free of charge!"))
                 {
-                    RegMatch = "seconds.(.*?)The following results are provided free of charge!";
+                    RegMatch = "seconds.(.*?)The following";
+                }
+                
+                foreach (Match watchTime in Regex.Matches(rektHtml, "Search completed in: (.*?) seconds.", RegexOptions.Singleline))
+                {
+                    var totalTime = watchTime.Groups[1];
                 }
 
                 foreach (Match allResults in Regex.Matches(rektHtml, RegMatch, RegexOptions.Singleline))
@@ -96,7 +110,7 @@ namespace ConsoleApplication1
                     String fuckHtml = allResults.Groups[1].Value.Replace("Subscribe today to unlock every result! As low as $0.76 a day!", "").Replace("hidden", "").Replace("This data was leaked on approximately ", "").Replace("has: ", ": ").Replace("More records found but ", "").Replace("00:00:00", "").Replace("0000-00-00", "").Replace(", 79+ databases in one", "").TrimEnd(Environment.NewLine.ToCharArray()).TrimStart(Environment.NewLine.ToCharArray());
                     Console.WriteLine(String.Join(Environment.NewLine, fuckHtml.Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Select(line => line.Split(':')).Select(items => String.Format("  {0,-30}| {1,3}", items[0].Trim(), Regex.Match(items[1], "[0-9]+").Value))));
 
-                    if (RegMatch != "seconds.(.*?)function")
+                    if (RegMatch != "onds.(.*?)func")
                     {
                         Console.WriteLine("  Mate1.com                     |   ?");
                     }
@@ -119,11 +133,12 @@ namespace ConsoleApplication1
                 }
                 Console.WriteLine();
             }
-            watch.Stop();
-            var elapsedMs = watch.ElapsedMilliseconds;
+
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"It took us only {elapsedMs} ms to look this up!");
+            Console.WriteLine($"It took us only {totalTime} ms to look this up!");
+            
             goto doAgain;
+            
         }
     }
 }
